b1862bdeb50076239fd73db203dfc25cb3529539
laoye_jnr <idrisaboladeo@gmail.com>
2021-04-20 08:43:44 +0100

    adds landing page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
      darkMode: false, 
      theme: {
+      screens: {
+        'sm': {'min': '320px', 'max': '580px'},
+        'md': {'min': '581px', 'max': '999px'},
+        'lg': {'min': '1000px', 'max': '1400px'},
+        'xl': {'min': '1280px', 'max': '1535px'},
+        '2xl': {'min': '1536px'},
+      },
        extend: {},
      },
      variants: {
